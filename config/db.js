474ecf1629e4c25ab381181ee3255a2b9738317c
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-08-02 22:08:09 +0545

    remove console

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB Connected");
+        
     } catch (e) {
-        console.log("MongoDB not connected", e);
+        
     }
 };
 

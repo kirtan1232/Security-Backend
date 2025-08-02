55ab4b66e172229e84e7df9e673975ba02e6afef
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-08-02 22:09:30 +0545

    remove console log

--- routes/auditLogRoutes.js
+++ routes/auditLogRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auditLogController = require('../controller/auditLogController');
-const authMiddleware = require('../middleware/auth'); // Ensure this checks JWT and sets req.user
+const authMiddleware = require('../middleware/auth'); 
 
 // Only admin can view logs
 router.get('/', authMiddleware, auditLogController.getAuditLogs);

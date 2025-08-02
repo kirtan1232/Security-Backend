c2d193d1132ec5ec4c23d912a2c6c872e396abfb
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-08-02 22:10:14 +0545

    remove console log

--- utils/passwordvalidator.js
+++ utils/passwordvalidator.js
@@ -1,5 +1,5 @@
 function isStrongPassword(password) {
-    // At least 8 characters, 1 uppercase, 1 lowercase, 1 number, 1 special char
+ 
     return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+={}[\]|\\:;"'<>,.?/~`]).{8,}$/.test(password);
 }
 module.exports = isStrongPassword;

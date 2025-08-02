b5ece3e25cc14c6f1b3d5d7b02e0c6d7d54e9876
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-08-02 22:09:36 +0545

    remove console log

--- model/Quiz.js
+++ model/Quiz.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const quizSchema = new mongoose.Schema({
     day: { type: String, required: true },
-    instrument: { type: String, required: true }, // New field for instrument
+    instrument: { type: String, required: true }, 
     quizzes: [
         {
             question: { type: String, required: true },
             options: { type: [String], required: true },
             correctAnswer: { type: String, required: true },
-            chordDiagram: { type: String, required: false }, // Make it optional
+            chordDiagram: { type: String, required: false }, 
         },
     ],
 }, { timestamps: true });
